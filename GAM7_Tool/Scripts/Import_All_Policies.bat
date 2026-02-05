82d62c88a049affc12e917ce6d7c91632bc30c94
rsaddul <52894448+rsaddul@users.noreply.github.com>
2026-02-05 11:40:37 +0000

    Delete GAM7_Tool directory

--- GAM7_Tool/Scripts/Import_All_Policies.bat
+++
@@ -1,7 +0,0 @@
-gam csv "%~dp0Import_All_Policies.csv" ^
-  gam update chromepolicy "~policyName" "~fieldName" "~fieldValue" orgunit "~orgUnitPath"
-
-echo.
-echo Closing window in 5 seconds...
-timeout /t 5 /nobreak >nul
-exit
